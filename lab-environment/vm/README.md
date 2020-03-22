39297dc51150ce610f4432bb8529fd2febb1fe0b
Wes Belt <wbelt@checkpoint.com>
2020-03-22 13:33:30 -0400

    Update outputs and readme for vm example

---
+++ lab-environment/vm/README.md
@@ -0,0 +1,6 @@
+# Create Lab VMs
+
+Create the following VMs:
+- A basic apache/php web server with a public IP address on the external_subnet
+- A dvwa web server with a public IP address on the external_subnet
+- A basic mysql database server on the internal_subnet with no public IP address
--- lab-environment/vm/outputs.tf
+++ lab-environment/vm/outputs.tf
@@ -1,8 +1,12 @@
-# output "web1_ip_address" {
-#   value = module.web1.ip_address
-#   description = "IP address for web1"
-# }
-# output "db1_ip_address" {
-#   value = module.db1.ip_address
-#   description = "IP address for db1"
-# }
+output "web1_ip_address" {
+  value = module.web1.ip_address
+  description = "IP address for web1"
+}
+output "web2_ip_address" {
+  value = module.web2.ip_address
+  description = "IP address for web2"
+}
+output "db1_ip_address" {
+  value = module.db1.ip_address
+  description = "IP address for db1"
+}
