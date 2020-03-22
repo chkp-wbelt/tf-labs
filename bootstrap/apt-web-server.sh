e62f10a1dddc303e7d90282cc8383794bb443edb
Wes Belt <wbelt@checkpoint.com>
2020-03-22 13:09:52 -0400

    Bootstrap cleanup

--- bootstrap/apt-web-server.sh
+++ bootstrap/apt-web-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get -y install apache2 php libapache2-mod-php
-curl -o /var/www/html/index.php https://labstorage813.blob.core.windows.net/lab-bootstrap/bootstrap/web1/index.php
-patch /etc/apache2/sites-available/000-default.conf <(curl -s https://labstorage813.blob.core.windows.net/lab-bootstrap/bootstrap/web1/apache-index-php.patch)
+curl -o /var/www/html/index.php https://raw.githubusercontent.com/chkp-wbelt/tf-labs/master/bootstrap/web1/index.php
+patch /etc/apache2/sites-available/000-default.conf <(curl -s https://raw.githubusercontent.com/chkp-wbelt/tf-labs/master/bootstrap/web1/apache-index-php.patch)
 systemctl restart apache2
--- bootstrap/web1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get upgrade
-apt-get -y install apache2 php libapache2-mod-php
-apt-get -y autoremove --purge
