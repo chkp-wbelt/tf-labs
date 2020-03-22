c39aa9bab1793f8c71fb53a6475ed4558cf5fbb6
Wes Belt <wbelt@checkpoint.com>
2020-03-22 14:44:19 -0400

    remove duplicate variables

--- init.tf
+++ init.tf
@@ -17,16 +17,6 @@ data "terraform_remote_state" "lab_network" {
       key                  = "lab-environment.tfstateenv:lab-network"
     }
 }
-variable "gw_instance_size" {
-    type = string
-    description = "Azure VM instance size for gateways"
-    default = "Standard_D2_v3" #cheapest minimum gateway size (two nics)
-}
-variable "vm_instance_size" {
-    type = string
-    description = "Azure VM instance size for virtual machines"
-    default = "Standard_B1ms" #cheapest instance size for basic vms
-}
 variable "ssh_key_data" {
     type = string
     description = "ssh key data for authentication, can be populated via TF_VAR_ssh_key_data environmental variable"
